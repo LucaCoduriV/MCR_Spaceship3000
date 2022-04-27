01e54e03a153b29761f83adcaeddea668783b249
Luca Coduri <coduri.vitor@gmail.com>
2022-04-27 21:09:40 +0200

    uml

--- core/src/ch/crepe/game/Spaceship3000.java
+++ core/src/ch/crepe/game/Spaceship3000.java
@@ -1,6 +1,7 @@
 package ch.crepe.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,18 +11,31 @@ public class Spaceship3000 extends ApplicationAdapter {
 	Texture img;
 	float x = 0;
 	float y = 0;
-	
+	float speedX = 2;
+	float speedY = 2;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("GetPersonaPhoto.jpg");
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		if(x > Gdx.graphics.getWidth() || x < 0) {
+			speedX = -speedX;
+		}
+		if(y > Gdx.graphics.getHeight() || y < 0) {
+			speedY = -speedY;
+		}
+
+		x += speedX;
+		y += speedY;
+
 		batch.begin();
-		batch.draw(img, x++, y++);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
