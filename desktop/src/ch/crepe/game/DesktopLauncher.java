0630dbc38a0e5b6ba2e073b13bad456e34724297
Luca Coduri <coduri.vitor@gmail.com>
2022-04-27 10:29:30 +0200

    Project init

---
+++ desktop/src/ch/crepe/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ch.crepe.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ch.crepe.game.Spaceship3000;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Spaceship3000");
+		new Lwjgl3Application(new Spaceship3000(), config);
+	}
+}
