eeaf828d7dc1f3fb4ae97c83782e117b44a40bef
chloefont <cfontaine@citycable.ch>
2022-06-01 10:49:16 +0200

    movement asteroids

--- core/src/ch/crepe/game/entities/Asteroid.java
+++ core/src/ch/crepe/game/entities/Asteroid.java
@@ -3,12 +3,15 @@ package ch.crepe.game.entities;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-public class Asteroid extends Entity {
+public class Asteroid extends VectorialMovableEntity {
+    static int SPEED = 2;
 
     public Asteroid(Vector2 position, Texture texture) {
-        super(position, texture);
+        super(position, texture, SPEED);
     }
 
     @Override
-    public void move() { }
+    public void move() {
+        vectorialMove(new Vector2(0, -1));
+    }
 }
