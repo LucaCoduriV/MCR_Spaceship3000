d55df37d1113eb6a1b3e4b9ebf36f7cd735f2325
Damiano <damiano.mondaini@gmail.com>
2022-06-19 20:58:48 +0200

    Commented BottomEntityCleaner.java

--- core/src/ch/crepe/game/BottomEntityCleaner.java
+++ core/src/ch/crepe/game/BottomEntityCleaner.java
@@ -6,11 +6,19 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.util.List;
 
+/**
+ * This class is a utility class to clean entities which are under the world bounds.
+ */
 public class BottomEntityCleaner extends EntityCleaner {
     public BottomEntityCleaner(List<Entity> entities, Rectangle bounds) {
         super(entities, bounds);
     }
 
+    /**
+     * Check if an entity is under the world bounds.
+     * @param entity The entity to check
+     * @return True if the entity is under the world bounds, false otherwise
+     */
     @Override
     protected boolean isOutOfBounds(Entity entity) {
         return entity.getHitbox().getCenter(new Vector2()).y < getBounds().y;
