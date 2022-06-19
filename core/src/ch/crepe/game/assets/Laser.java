0db8b93cb295331de9bafc713bfd332a0204b184
Damiano <damiano.mondaini@gmail.com>
2022-06-19 22:07:02 +0200

    AssetsLoader.java

--- core/src/ch/crepe/game/assets/Laser.java
+++ core/src/ch/crepe/game/assets/Laser.java
@@ -1,18 +1,10 @@
 package ch.crepe.game.assets;
 
 public enum Laser implements AssetPath {
-    blueSimple("weapons/lasers/blue_simple.png"),
     blueFast("weapons/lasers/blue_fast.png"),
     bluePlasma("weapons/lasers/blue_plasma.png"),
-    blueElectric("weapons/lasers/blue_electric.png"),
-    greenSimple("weapons/lasers/green_simple.png"),
-    greenFast("weapons/lasers/green_fast.png"),
     greenPlasma("weapons/lasers/green_plasma.png"),
-    greenElectric("weapons/lasers/green_electric.png"),
-    redSimple("weapons/lasers/red_simple.png"),
-    redFast("weapons/lasers/red_fast.png"),
-    redPlasma("weapons/lasers/red_plasma.png"),
-    redElectric("weapons/lasers/red_electric.png");
+    greenElectric("weapons/lasers/green_electric.png");
 
     private final String path;
     Laser(String path) {
--- core/src/ch/crepe/game/assets/SpaceShip.java
+++ core/src/ch/crepe/game/assets/SpaceShip.java
@@ -13,26 +13,6 @@ public enum SpaceShip implements AssetPath {
     paintUser("ships/paint/user-spaceship.png"),
     paintAi("ships/paint/ai-spaceship.png");
 
-
-
-    /*
-    arcadeBlueAsteroid("asteroids/arcade/asteroid-blue.png"),
-    arcadeGreenAsteroid("asteroids/arcade/asteroid-green.png"),
-
-    speedsterSimple("ships/starships_0000_Ice-Speedster-simple.png"),
-    sunKillerSimple("ships/starships_0000_sun-killer-simple.png"),
-
-    centenialHawkSimple("ships/starships_0001_Centenial-Hawk-simple.png"),
-    sunKiller("ships/starships_0001_Sun-killer.png"),
-    bowFighterSimple("ships/starships_0002_bow-fighter-simple.png"),
-    exVingSimple("ships/starships_0003_Ex-ving-simple.png"),
-    iceSpeedster("ships/starships_0004_Ice-Speedster.png"),
-    centenialHawk("ships/starships_0005_Centenial-Hawk.png"),
-    bowFighter("ships/starships_0006_Bow-fighter.png"),
-    bowFighterDebug("ships/starships_0006_Bow-fighter_debug.png"),
-    exVing("ships/starships_0007_Ex-ving.png");
-     */
-
     private final String path;
 
     SpaceShip(String path) {
